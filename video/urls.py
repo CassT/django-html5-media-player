527e00e110da217beda88ac5c45070af8c7959f5
Cassidy Thompson <cassdamthompson@gmail.com>
2015-03-12 21:29:39 -0600

    update css, urls

--- video/urls.py
+++ video/urls.py
@@ -5,5 +5,5 @@ from video import views
   
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<video_src>[\w.,/_\- ]+)$', views.index, name='index'),
+    url(r"^(?P<video_src>[\w.,/_\-\' ]+)$", views.index, name='index'),
 )
