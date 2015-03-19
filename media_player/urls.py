bfcfe72d66e41d0eae5554b0b0edde4706f72b17
Cassidy Thompson <cassdamthompson@gmail.com>
2015-03-19 04:51:38 -0600

    updated css and urls, views

--- media_player/urls.py
+++ media_player/urls.py
@@ -3,6 +3,5 @@ from media_player import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r"^(?P<media_src>[\w.,/_\-\'\!\ ]+)$", views.media_player, name='media_player'),
+    url(r"^(?P<media_src>[\w.,/_\-\'\!\[\] ]+)$", views.media_player, name='media_player'),
 )
--- media_player/views.py
+++ media_player/views.py
@@ -5,26 +5,24 @@ from django.contrib.auth.decorators import login_required
 from models import MediaServer
 
 def display_directory(path, server, doc):
+    tag = doc.tag
+    text = doc.text
     if os.path.isdir(path):
         cssid = path[len(server.directory_path):]
         doc.asis('<label for="'+cssid+'">')
-        doc.text(os.path.basename(path))
+        text(os.path.basename(path))
         doc.asis('</label>')
         doc.asis('<input type="checkbox" id="'+cssid+'">')
-        with doc.tag('ul', klass='collapsibleList'):
+        with tag('ul', klass='collapsibleList'):
             for x in sorted(os.listdir(path)):
-                with doc.tag('li'):
+                with tag('li'):
                     display_directory(os.path.join(path,x), server, doc)
     else:
         url_paths = {'V':'/media_player/video/',
                      'A':'/media_player/audio/'}
         urlpath = url_paths[server.media_type] + server.name + path[len(server.directory_path):]
-        with doc.tag('a', href=urlpath):
-            doc.text(os.path.splitext(os.path.basename(path))[0])
-
-
-def index(request):
-    return render(request)
+        with tag('a', href=urlpath):
+            text(os.path.splitext(os.path.basename(path))[0])
 
 @login_required
 def media_player(request, media_src=''):
@@ -32,6 +30,7 @@ def media_player(request, media_src=''):
     types = {'video':'V', 'audio':'A'}
     requested_type = media_src.split('/')[0]
     media_servers = MediaServer.objects.filter(media_type=types[requested_type])
+    image_src = ''
     if len(media_src.split('/')) >= 2:
         requested_server = media_src.split('/')[1]
     else:
@@ -39,6 +38,14 @@ def media_player(request, media_src=''):
     for server in media_servers:
         if server.name == requested_server:
             media_src = server.server_url + media_src[len(requested_type + '/' + requested_server):]
+            parent_dir = server.directory_path + os.path.split(media_src)[0][len(server.server_url):]
+            for item in os.listdir(parent_dir):
+                if os.path.splitext(item)[1].lower() in ['.jpg','.png']:
+                    image_src = os.path.join(os.path.split(media_src)[0],item)
         display_directory(server.directory_path, server, doc)
-    context = {'medialisting':doc.getvalue(), 'mediasource':media_src, 'mediatype':requested_type}
+    context = { 'medialisting':doc.getvalue(), 
+                'mediasource':media_src, 
+                'mediatype':requested_type, 
+                'imagesource':image_src,
+              }
     return render(request, 'media_player/media_player.html', context)
